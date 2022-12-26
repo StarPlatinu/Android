025b35d0eb07ce37fec06eef24ba38d738a67672
ThanhVu <thanhvp1509@gmail.com>
2022-12-26 11:21:29 +0700

    Update

--- MyApplication/app/src/main/java/com/training/MainActivity.java
+++ MyApplication/app/src/main/java/com/training/MainActivity.java
@@ -3,12 +3,22 @@ package com.training;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Spinner sp,sp1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.test5);
+        sp = findViewById(R.id.sp2);
+        sp1 = findViewById(R.id.sp1);
+        String[] list = {"FPT","PTIT","HUST","NEU"};
+        ArrayAdapter adapter = new ArrayAdapter(this,R.layout.item,list);
+        sp.setAdapter(adapter);
+        String[] list1 = getResources().getStringArray(R.array.country);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this,R.layout.item,list1);
+        sp1.setAdapter(adapter1);
     }
 }
